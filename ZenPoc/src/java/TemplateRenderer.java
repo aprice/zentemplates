9c04543244949c3f9ecd29c8ab924d79845da79e
Adrian Price <adrian.price@gmail.com>
2013-04-10 22:00:20 -0400

    Revert "Revert "Expanded derivation functionality""
    This reverts commit 1b77e27f4dedb66c57f862ac4692aeb2585fdd55.

--- ZenPoc/src/java/TemplateRenderer.java
+++ ZenPoc/src/java/TemplateRenderer.java
@@ -1,11 +1,10 @@
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Writer;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringEscapeUtils;
@@ -20,18 +19,21 @@ import org.jsoup.select.Elements;
  * @author Adrian
  */
 public class TemplateRenderer {
-
 	File templateFile;
 	Document inDoc, outDoc;
 	Map<String, Object> model = new HashMap<String, Object>();
-	String result;
+	String result, templateRoot = null, siteRoot = null;
 
-	TemplateRenderer(String templatePath, ServletContext servletContext) {
+	public TemplateRenderer(String templatePath, ServletContext servletContext) {
 		this(servletContext.getRealPath(templatePath));
 	}
 
-	private TemplateRenderer(String templatePath) {
-		templateFile = new File(templatePath);
+	public TemplateRenderer(String templatePath) {
+		this(new File(templatePath));
+	}
+
+	public TemplateRenderer(File templateFile) {
+		this.templateFile = templateFile;
 		result = "";
 	}
 
@@ -40,117 +42,180 @@ public class TemplateRenderer {
 	}
 
 	void render(HttpServletResponse response) throws IOException {
-		long start = System.currentTimeMillis();
+		handleRendering();
+		writeOut(response);
+	}
+
+	void render(Writer out) throws IOException {
+		handleRendering();
+		writeOut(out);
+	}
+
+	private void handleRendering() throws IOException {
+		System.out.println("Rendering template");
 		loadTemplate();
 		handleDerivation();
 		handleInjection();
 		handleSubstitution();
-		writeOut(response);
-		long end = System.currentTimeMillis();
-		System.out.println("Rendering time: "+(end - start)+" ms.");
 	}
 
 	private void loadTemplate() throws IOException {
-		System.out.println("Loading template :" + templateFile);
-		long start = System.currentTimeMillis();
+		System.out.println("Loading template: " + templateFile);
 		inDoc = Jsoup.parse(templateFile, "UTF-8");
 		outDoc = new Document(templateFile.getParent());
-		long end = System.currentTimeMillis();
-		System.out.println("Load time: "+(end - start)+" ms.");
 	}
 
 	private void handleDerivation() throws IOException {
-		long start = System.currentTimeMillis();
 		Element inHtml = inDoc.child(0);
-		String derivesFrom = inHtml.attr("class");
-		if (derivesFrom.contains("_derivesFrom(")) {
+
+		// Locate derived parent
+		String derivesFrom = inHtml.dataset().get("derivesfrom");
+		if (derivesFrom == null) {
+			derivesFrom = inHtml.attr("class");
+			if (!derivesFrom.isEmpty()) {
+				String[] candidates = derivesFrom.split("\\s");
+				for (String parent : candidates) {
+					File derivedFile = findDerivedFile(parent + ".html");
+					if (derivedFile != null) {
+						derivesFrom = derivedFile.getPath();
+						break;
+					}
+				}
+			}
+		} else {
+			File derivedFile = findDerivedFile(derivesFrom);
+			derivesFrom = (derivedFile == null) ? null : derivedFile.getPath();
+		}
+
+		if (derivesFrom != null && !derivesFrom.isEmpty()) {
 			System.out.println("Handling derivation: " + derivesFrom);
-			// Get parent file name
-			Pattern pat = Pattern.compile("_derivesFrom\\('?([^\\)']+)'?\\)");
-			Matcher match = pat.matcher(derivesFrom);
-			if (match.matches()) {
-				String parent = match.group(1);
-				if (!(new File(parent).isAbsolute())) {
-					parent = templateFile.getParent() + File.separator + parent;
+
+			// Handle nested derivation
+			TemplateRenderer parentRenderer = new TemplateRenderer(derivesFrom);
+			parentRenderer.loadTemplate();
+			parentRenderer.handleDerivation();
+			outDoc = parentRenderer.outDoc;
+
+			// Handle derived elements
+			Elements outPlaceholders = outDoc.getElementsByAttribute("id");
+			for (Element outPlaceholder : outPlaceholders) {
+				Element inReplacement = inDoc.getElementById(outPlaceholder.id());
+				if (inReplacement != null) {
+					outPlaceholder.replaceWith(inReplacement);
+					if (outPlaceholder.id().endsWith("?")) {
+						outPlaceholder.attr("id", outPlaceholder.id().substring(0, outPlaceholder.id().length()-1));
+					}
+				} else if (outPlaceholder.id().endsWith("?")) {
+					outPlaceholder.remove();
+				}
+			}
+
+			// Handle appended elements
+			Elements inAppendices = inDoc.getElementsByAttribute("data-z-append");
+			for (Element inAppendix : inAppendices) {
+				Element inParent = inAppendix.parent();
+				Element outParent = null;
+				if (inParent.tagName().equals("head") || inParent.tagName().equals("body")) {
+					outParent = outDoc.getElementsByTag(inParent.tagName()).get(0);
+				} else if (!inParent.id().isEmpty()) {
+					outParent = outDoc.getElementById(inParent.id());
 				}
 
-				// Handle nested derivation
-				TemplateRenderer parentRenderer = new TemplateRenderer(parent);
-				parentRenderer.loadTemplate();
-				parentRenderer.handleDerivation();
-				outDoc = parentRenderer.outDoc;
+				if (outParent == null) {
+					System.out.println("Notice: no parent found for appendix: "+inAppendix.toString());
+					continue;
+				}
 
-				// Handle derived elements
-				Elements placeholders = outDoc.getElementsByAttribute("id");
-				for (Element placeholder : placeholders) {
-					Element replacement = inDoc.getElementById(placeholder.id());
-					if (replacement != null) {
-						placeholder.replaceWith(replacement);
-						if (placeholder.id().endsWith("?")) {
-							placeholder.attr("id", placeholder.id().substring(0, placeholder.id().length()-1));
-						}
-					} else if (placeholder.id().endsWith("?")) {
-						placeholder.remove();
-					}
+				String[] appendixData = inAppendix.dataset().get("z-append").split(":",2);
+
+				Element outSibling = outParent.getElementById(appendixData[1]);
+				if (outSibling == null) {
+					System.out.println("Notice: sibling not found: "+appendixData[1]);
+					continue;
+				}
+
+				if (appendixData[0].equals("before")) {
+					outSibling.before(inAppendix);
+				} else if (appendixData[0].equals("after")) {
+					outSibling.after(inAppendix);
 				}
 			}
 		} else {
 			outDoc = inDoc.clone();
 		}
-		long end = System.currentTimeMillis();
-		System.out.println("Derivation time: "+(end - start)+" ms.");
 	}
 
 	private void handleInjection() {
 		System.out.println("Handling injection");
-		long start = System.currentTimeMillis();
 		for (String key : model.keySet()) {
-			Elements matches = outDoc.getElementsByClass(key);
-			for (Element match : matches) {
+			Elements outMatches = outDoc.getElementsByClass(key);
+			for (Element outMatch : outMatches) {
 				if (model.get(key) instanceof Collection) {
-					Element lastElement = match;
+					Element lastElement = outMatch;
 					for (Object item : ((Collection)model.get(key))) {
-						Element newElement = match.clone();
+						Element newElement = outMatch.clone();
 						newElement.html(cleanAndParagraph(item.toString()));
 						lastElement.after(newElement);
 						lastElement = newElement;
 					}
-					match.remove();
+					outMatch.remove();
 				} else {
-					match.html(cleanAndParagraph(model.get(key).toString()));
+					outMatch.html(cleanAndParagraph(model.get(key).toString()));
 				}
 			}
 		}
-		long end = System.currentTimeMillis();
-		System.out.println("Injection time: "+(end - start)+" ms.");
 	}
 
 	private void handleSubstitution() {
 		System.out.println("Handling substitution");
-		long start = System.currentTimeMillis();
 		StrSubstitutor ss = new StrSubstitutor(model);
 		String text = ss.replace(outDoc.html());
 		// TODO: cleanHtml(model.get(key).toString()));
-		// Requires implementing a custom StrLookup
+		// Requires implementing a custom StrLookup or decorating an existing StrLookup
 
 		outDoc = Jsoup.parse(text);
-		long end = System.currentTimeMillis();
-		System.out.println("Substitution time: "+(end - start)+" ms.");
 	}
 
 	private void writeOut(HttpServletResponse response) throws IOException {
 		System.out.println("Flushing output");
-		long start = System.currentTimeMillis();
 		outDoc.outputSettings().indentAmount(4);
 		result = outDoc.ownerDocument().html();
 		response.setContentType("text/html;charset=utf-8");
 		response.getWriter().write(result);
-		long end = System.currentTimeMillis();
-		System.out.println("Write time: "+(end - start)+" ms.");
+	}
+
+	private void writeOut(Writer out) throws IOException {
+		System.out.println("Flushing output");
+		outDoc.outputSettings().indentAmount(4);
+		result = outDoc.ownerDocument().html();
+		out.write(result);
+	}
+
+	private File findDerivedFile(String name) {
+		File f = new File(name);
+		if (f.isAbsolute()) {
+			return f.exists() ? f : null;
+		} else {
+			// Check in master template path, if defined
+			if (templateRoot != null) {
+				f = new File(templateRoot + File.separatorChar + name);
+				if (f.exists()) return f;
+			}
+
+			// Check in site root path, if defined
+			if (siteRoot != null) {
+				f = new File(siteRoot + File.separatorChar + name);
+				if (f.exists()) return f;
+			}
+
+			// Check in template directory
+			f = new File(templateFile.getParent() + File.separatorChar + name);
+			return f.exists() ? f : null;
+		}
 	}
 
 	private String cleanHtml(String html) {
-		String out = html.replaceAll("[ \\t]+", " ");
+		String out = html.trim().replaceAll("[ \\t]+", " ");
 		return StringEscapeUtils.escapeXml(out);
 	}
 
@@ -164,5 +229,25 @@ public class TemplateRenderer {
 
 	private String cleanAndParagraph(String text) {
 		return newlineToParagraph(cleanHtml(text));
+	}
+
+	public String getTemplateRoot() {
+		return templateRoot;
+	}
+
+	public void setTemplateRoot(String templateRoot) {
+		if (!new File(templateRoot).isAbsolute()) {
+			this.templateRoot = templateFile.getParent() + File.separatorChar + templateRoot;
+		} else {
+			this.templateRoot = templateRoot;
+		}
+	}
+
+	public String getSiteRoot() {
+		return siteRoot;
+	}
+
+	public void setSiteRoot(String siteRoot) {
+		this.siteRoot = siteRoot;
 	}
 }
